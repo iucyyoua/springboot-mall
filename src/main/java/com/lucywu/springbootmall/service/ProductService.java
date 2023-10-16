cd0894c6072a16476823a5ffe3dc02050d966332
LAPTOP-17R46N7O\iucyy <iucyyoua@gmail.com>
2023-10-16 20:57:40 +0800

    商品功能-實作新增商品

--- src/main/java/com/lucywu/springbootmall/service/ProductService.java
+++ src/main/java/com/lucywu/springbootmall/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.lucywu.springbootmall.service;
 
+import com.lucywu.springbootmall.dto.ProductRequest;
 import com.lucywu.springbootmall.model.Product;
 
 public interface ProductService {
 
     //根據producyID取得商品資訊
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
